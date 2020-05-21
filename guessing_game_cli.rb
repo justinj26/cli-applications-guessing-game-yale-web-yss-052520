3caf0e3f83286551390399981ec1aadb5b26f267
Justin James <justin.james@yale.edu>
2020-05-21 07:24:28 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,9 +4,9 @@ def run_guessing_game
   puts "Guess a number between 1 and 6"
   input = gets.chomp 
   if input == rand6
-    puts "You guess the correct number!"
+    return puts "You guess the correct number!"
   elsif input == "exit"
-    puts "Goodbye!"
+    return puts "Goodbye!"
   else 
     puts "Sorry! The computer guessed #{rand6}"
   end 
